9f4ba490865b6974f4d62ca8b7d4afa09aad1743
unknown <cymmer4@gmail.com>
2022-07-15 02:02:21 +0800

    Edits code

--- app.js
+++ app.js
@@ -4,15 +4,15 @@ const port = 3000;
 var exec = require('child_process').exec;
 
 app.get('/cAndCPP', (req, res) => {
-
-    exec('apt upgrade', (error, stdout, stderr) => {
+    let res = '';
+    exec('pkg install clang -y', (error, stdout, stderr) => {
         if(!error) {
-            console.log("STDOUT: ", stdout);
+            res = "STDOUT: " + stdout;
         } else {
-            console.log("ERROR: ", error, stderr);
+            res = "ERROR: " +  error + stderr;
         }
     })
-  res.send('Hello World!')
+  res.send('Hello World! ' + stdout);
 })
 
 app.listen(port, () => {
